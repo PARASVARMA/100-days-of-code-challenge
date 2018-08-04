a6f00a4ef3edabd8b543f2bee2ead0d148e9bd83
PARASVARMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-04 22:53:59 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,33 @@
+#print the hello message
+print ("hello python")
+
+#This is how we can concatenate of strings
+full_name = "paras verma"
+print ("hello" + " " +  full_name)
+
+#How we can find the index from any string
+print (full_name.index("r"))
+
+#Replacing the substring
+print(full_name.replace("paras","piyush"))
+
+#Basic operations on numbers
+print (2+3)
+print (4*3+2)
+print (4*(3+2))
+
+#assignment to variable
+my_fav = 10
+print (str(my_fav) + "is my favorite no.")
+
+#finding absolute value
+print (abs(-4))
+
+#finding exponential
+print (pow(2,3))
+
+#finding round off
+print (round(2.5))
+print (round(2.2))
+print (round(2202.7))
+
