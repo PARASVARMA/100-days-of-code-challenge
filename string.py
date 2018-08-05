367d756f39e9437d514a95bc7581d5ef46dec354
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-05 22:49:18 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,10 @@
+#this is how string prints in python
+print(1 + 10)
+print("hello\npython")
+
+#Concatenation
+6print("First string" + "," + "Second string")
+print("2" + "3")
+
+#String Operation
+print("paras" * 3).
