10860b5fbe496f2c21b589ecd048effb020920fd
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-05 22:51:28 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,30 @@
+#how we can declare and initialize any variable in python
+x = 5
+print(x)
+print(x+3)
+print(x)
+
+#variable can be assigned many times
+x = 125.6
+print(x)
+x = "This is a string"
+print(x + "!")
+
+#Example:-
+spam = 25
+eggs = 3
+del spam
+eggs = 4
+spam = 5
+print(spam*eggs)
+
+#In-place Operator
+x = 2
+print(x)
+x += 3
+print(x)
+
+#example
+x = "a"
+x *= 3
+print(x)
