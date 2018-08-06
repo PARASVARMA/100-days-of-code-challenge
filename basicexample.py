dfa777edff0f2e1d0b0c788ed33aa2bf764af9e8
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-06 22:31:58 +0530

    Create basicexample.py

---
+++ basicexample.py
@@ -0,0 +1,16 @@
+#first example
+spam = "7"
+spam = spam + "0"
+eggs = int(spam) + 3
+print(float(eggs))
+
+#second example
+x = 5
+y = x + 3
+y = int(str(y) + "2")
+print(y)
+
+#Third example
+word = input("Enter a word")
+print(word)
+print(word + 'shop')
