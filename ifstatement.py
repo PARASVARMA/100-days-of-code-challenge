bf95917239dc3455ca71e6ad20a03f1268dc4577
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-07 22:49:48 +0530

    Create ifstatement.py

---
+++ ifstatement.py
@@ -0,0 +1,11 @@
+num = 7
+if num == 5:
+    print("number is 5")
+else:
+    if num == 11:
+        print("number is 11")
+    else:
+     if num == 7:
+         print("number is 7")
+     else:
+         print("number is not 5,7 and 11")
