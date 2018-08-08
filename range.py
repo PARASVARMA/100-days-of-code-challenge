24b807cfff3865f18d43fd53dbfdca2359673a8d
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-08 23:22:48 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,12 @@
+#range function
+numbers = list(range(10))
+print(numbers)
+
+#other example of range function
+nums = list(range(5,8))
+print(len(nums))
+
+
+#other one also exmaple of range function
+nums = list(range(3,15,3))
+print(nums[2])
