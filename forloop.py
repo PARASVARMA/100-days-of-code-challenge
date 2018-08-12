f9dec1beff11e0b32111384ab62ba00f8423cbb5
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-12 23:32:22 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,10 @@
+#for loop introduction:-
+words = ["hello", "world", "spam", "eggs"]
+for word in words:
+    print(word + "!")
+
+#for loop for range objects:-
+for i in range(5):
+    print("hello!")
+
+
