dbab4e2cd3ae22df1539dbb0c66d45c87f278f4c
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-14 23:00:35 +0530

    Create assertion.py

---
+++ assertion.py
@@ -0,0 +1,15 @@
+#Assertion:
+print(1)
+assert 2 + 2 == 4
+print(2)
+assert 1 + 1 == 3
+print(3)
+
+#other one example of assertion:-
+print(0)
+assert "h" != "w"
+print (1)
+assert False
+print(2)
+assert True
+print(3)
