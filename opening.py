269dc01aeb262f47975dcc51eae3330fb73ecb68
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-15 23:41:55 +0530

    Add files via upload

---
+++ opening.py
@@ -0,0 +1,15 @@
+# write mode
+open("filename.txt", "w")
+
+# read mode
+open("filename.txt", "r")
+open("filename.txt")
+
+# binary write mode
+open("filename.txt", "wb")
+
+#Opening Files
+file = open("filename.txt", "w")
+#do stuff to the file
+file.close()
+
