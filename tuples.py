e3469eed20d1c7719fd4284c9ba4d755564961ad
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-17 23:42:34 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,18 @@
+#Tuples:-
+words = ("spam","eggs","bread")
+print(words[1])
+
+#difference of syntax of list,dictionary and tuple:
+# list
+list =["one", "two"]
+
+# dictionary
+dict ={1: "one", 2: "two"}
+
+# tuple
+tp =("one", "two")
+
+#other exmple of tuples:=
+my_tuple = "one", "two", "three"
+print(my_tuple[0])
+
