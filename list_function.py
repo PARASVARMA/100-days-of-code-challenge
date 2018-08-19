c5940620e1d1f0a9450738aadd402f72b5525fe9
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-19 22:15:22 +0530

    Create list_function.py

---
+++ list_function.py
@@ -0,0 +1,10 @@
+#List Function:-
+nums = [55, 44, 33, 22, 11]
+if all([i > 5 for i in nums]):
+    print("All larger than 5")
+
+if any([i % 2 == 0 for i in nums]):
+    print("At least one is even")
+
+    for v in enumerate(nums):
+        print(v)
