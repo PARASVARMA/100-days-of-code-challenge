6c4a59533e6327b19c5939358a343cf93f3f3302
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-19 22:13:23 +0530

    Create useful_function.py

---
+++ useful_function.py
@@ -0,0 +1,22 @@
+#String functions here:-
+
+#join function-
+print(",".join(["spam","eggs","ham"]))
+
+#replace function-
+print("Hello ME".replace("ME","world"))
+
+#startswith function-
+print("This is a sentence.".startswith("This"))
+
+#endswith function-
+print("This is a sentence.".endswith("sentence."))
+
+#upper function-
+print("This is a sentence.".upper())
+
+#lower function-
+print("ALL CAPS SENTENCE.".lower())
+
+#split function-
+print("spam,eggs,ham".split(","))
