1129f6fadf40bd9ae8d06e7600b7a44df480e1fe
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-22 22:48:34 +0530

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,27 @@
+#Recursion:-
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x - 1)
+print(factorial(5))
+
+
+
+#other example of recursion:-
+def factorial(x):
+    return x * factorial(x - 1)
+print(factorial(5))
+
+
+#second one example of recursion:-
+def is_even(x):
+  if x == 0:
+    return True
+  else:
+    return is_odd(x-1)
+
+def is_odd(x):
+  return not is_even(x)
+print(is_odd(17))
+print(is_even(23))
