2a799c1403fe5cd6b6fd338955f7b24e9e8788eb
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-27 19:21:50 +0530

    Add files via upload

---
+++ repeatation_metachar.py
@@ -0,0 +1,31 @@
+#Repeatation metacharacter:-
+import re
+
+pattern = r"g+"
+
+if re.match(pattern, "g"):
+   print("Match 1")
+
+if re.match(pattern, "gggggggggggggg"):
+   print("Match 2")
+
+if re.match(pattern, "abc"):
+   print("Match 3")
+
+
+#? Metacharacter:-
+import re
+
+pattern = r"ice(-)?cream"
+
+if re.match(pattern, "ice-cream"):
+   print("Match 1")
+
+if re.match(pattern, "icecream"):
+   print("Match 2")
+
+if re.match(pattern, "sausages"):
+   print("Match 3")
+
+if re.match(pattern, "ice--ice"):
+   print("Match 4")
