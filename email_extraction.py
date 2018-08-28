ae9f0f93b04436b6a8d636362b3c8180a7be3368
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-28 22:00:53 +0530

    Add files via upload

---
+++ email_extraction.py
@@ -0,0 +1,9 @@
+#Email extraction:-
+import re
+
+pattern = r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)"
+str = "Please contact info@sololearn.com for assistance"
+
+match = re.search(pattern, str)
+if match:
+   print(match.group())
