1790945f2122c3a6e745ac62594718e05d39d908
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-08-29 21:31:07 +0530

    Add files via upload

---
+++ typle_unpacking.py
@@ -0,0 +1,13 @@
+#Tuple unpacking:-
+numbers = (1, 2, 3)
+a, b, c = numbers
+print(a)
+print(b)
+print(c)
+
+#other example of tuple unpacking:
+a, b, *c, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a)
+print(b)
+print(c)
+print(d)
