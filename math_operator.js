93d0ae552a29c9e68c6763b891383cb5f29d0b1f
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-06 21:59:47 +0530

    Create math_operator.js

---
+++ math_operator.js
@@ -0,0 +1,14 @@
+#Math operator are used here:
+
+
+#multiplication operator?:
+x = 10 * 5;
+document.write(x);
+
+#Division operator:
+var x = 100 / 5;
+document.write(x);
+
+#Modulus operator:
+var myVariable = 26 % 6;
+
