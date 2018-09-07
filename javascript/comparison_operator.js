999abb2506e8ea764ed534c3f6684d433e1266fb
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-07 21:06:59 +0530

    Create comparison_operator.js

---
+++ javascript/comparison_operator.js
@@ -0,0 +1,16 @@
+#Comparison Operator:-
+var num = 10; 
+// num == 8 will return false
+
+#other comparison operator:
+#equal operator:
+val1==val2 // are equal
+
+#not equal operator:
+val1!=val2 // not equal
+
+#less than operator:
+val1<val2 // less than
+
+#Strict equal operator:
+val1===val2 // are strict equal (identical)
