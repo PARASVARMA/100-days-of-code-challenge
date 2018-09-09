aea5520fcf3d5184b70f3e0e6d66a6e27270317a
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-09 22:17:43 +0530

    Create elseif_statement.js

---
+++ javascript/elseif_statement.js
@@ -0,0 +1,9 @@
+#else-if Statement:-
+var course = 3;
+if (course == 1) {
+   document.write("<h1>HTML Tutorial</h1>");
+} else if (course == 2) {
+   document.write("<h1>CSS Tutorial</h1>");
+} else {
+   document.write("<h1>JavaScript Tutorial</h1>");
+}
