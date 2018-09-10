4e3d074f514fcc33f454dda57898437e0b2f14d8
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-10 22:25:43 +0530

    Create switch_statement_break.js

---
+++ javascript/switch_statement_break.js
@@ -0,0 +1,18 @@
+#Switch statement with break:-
+var color ="yellow";
+switch(color) {
+   case "blue": 
+     document.write("This is blue.");
+     break;
+   case "red": 
+     document.write("This is red.");
+     break;
+   case "green": 
+     document.write("This is green."); 
+     break;
+   case "orange": 
+      document.write("This is orange."); 
+      break;
+   default: 
+      document.write("Color not found.");
+}
