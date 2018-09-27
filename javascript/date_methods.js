1575e3ba89ded224c8a757be768c0079871e395c
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-27 22:16:15 +0530

    Create date_methods.js

---
+++ javascript/date_methods.js
@@ -0,0 +1,10 @@
+//This is how we can use the date methods in javascript:-
+
+function printTime() {
+  var d = new Date();
+  var hours = d.getHours();
+  var mins = d.getMinutes();
+  var secs = d.getSeconds();
+  document.body.innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
