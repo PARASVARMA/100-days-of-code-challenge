fc662ca79251bc6703d0388951b9fd385bd9255e
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-27 22:13:49 +0530

    Create date_object.js

---
+++ javascript/date_object.js
@@ -0,0 +1,16 @@
+//Date Object examples here:-
+
+//Fri Jan 02 1970 00:00:00
+var d1 = new Date(86400000); 
+
+//Fri Jan 02 2015 10:42:00
+var d2 = new Date("January 2, 2015 10:42:00");
+
+//Sat Jun 11 1988 11:42:00
+var d3 = new Date(88,5,11,11,42,0,0);
+
+
+//Specified date and time:
+new Date(milliseconds)
+new Date(dateString)
+new Date(year, month, day, hours, minutes, seconds, milliseconds)
