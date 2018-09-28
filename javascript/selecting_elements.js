aa95b8e063fde256d0005c429163c027c8b78f73
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-28 22:46:17 +0530

    Create selecting_elements.js

---
+++ javascript/selecting_elements.js
@@ -0,0 +1,10 @@
+//Dom and Events:-
+
+//finds element by id
+document.getElementById(id) 
+
+//finds elements by class name
+document.getElementsByClassName(name) 
+
+//finds elements by tag name
+document.getElementsByTagName(name)
