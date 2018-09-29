6eac3bfbd50936845bc83cd1ec3b8c389821ad46
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-29 21:56:14 +0530

    Create changing_style.js

---
+++ javascript/changing_style.js
@@ -0,0 +1,7 @@
+//Changing Style element:-
+<div id="demo" style="width:200px">some text</div>
+<script>
+  var x = document.getElementById("demo");
+  x.style.color = "6600FF";
+  x.style.width = "100px";
+</script>
