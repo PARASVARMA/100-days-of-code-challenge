1dc7268853123e7062414699403ddc84f7511710
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-09-30 21:17:03 +0530

    Create removing_elements.js

---
+++ javascript/removing_elements.js
@@ -0,0 +1,11 @@
+#Removing Elements in Javascript:-
+<div id="demo">
+  <p id="p1">This is a paragraph.</p>
+  <p id="p2">This is another paragraph.</p>
+</div>
+
+<script>
+var parent = document.getElementById("demo");
+var child = document.getElementById("p1");
+parent.removeChild(child);
+</script>
