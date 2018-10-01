9dbae0f32b71dcfb25da0bbfe1293eebb5246854
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-01 19:25:38 +0530

    Create animation_3.js

---
+++ javascript/animation_3.js
@@ -0,0 +1,10 @@
+//Creating Animation by clear Interval in Javascript:-
+function move() {
+  if(pos >= 150) {
+    clearInterval(t);
+  }
+  else {
+    pos += 1;
+    box.style.left = pos+"px";
+  }
+}
