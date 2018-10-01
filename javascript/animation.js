ed2c61637f678ae89473a69bbee3c00935fe8a36
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-01 19:20:05 +0530

    Create animation.js

---
+++ javascript/animation.js
@@ -0,0 +1,18 @@
+//Creating Animation in Javascript:-
+<style>
+#container {
+  width: 200px;
+  height: 200px;
+  background: green;
+  position: relative;
+}
+#box {
+  width: 50px;
+  height: 50px;
+  background: red;
+  position: absolute;
+}
+</style>
+<div id="container">
+   <div id="box"> </div>
+</div>
