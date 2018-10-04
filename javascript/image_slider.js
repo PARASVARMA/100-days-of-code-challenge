c72f90f58c3788a4edb102964f3451635104373a
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-04 19:26:05 +0530

    Create image_slider.js

---
+++ javascript/image_slider.js
@@ -0,0 +1,7 @@
+//This is how we can create a image slider in Javascript:-
+<div>
+  <button> Prev </button>
+  <img id="slider" src="http://www.sololearn.com/uploads/slider/1.jpg" 
+    width="200px" height="100px"/>
+  <button> Next </button>
+</div>
