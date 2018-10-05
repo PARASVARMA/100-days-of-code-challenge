117c605fed5be56d85b2b3026d398e9ebddb95d8
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-05 21:46:29 +0530

    Create validation_function.js

---
+++ javascript/validation_function.js
@@ -0,0 +1,12 @@
+//Function validate for Validation form in Javascript:
+function validate() {
+  var n1 = document.getElementById("num1");
+  var n2 = document.getElementById("num2");
+  if(n1.value != "" && n2.value != "") {
+    if(n1.value == n2.value) {
+      return true;
+    }
+  }
+  alert("The values should be equal and not blank");
+  return false;
+}
