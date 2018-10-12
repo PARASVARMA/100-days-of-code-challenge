7a02fdd65b8507ac36baddf5bdc1b58c87ef51d7
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-12 21:39:50 +0530

    Create arithmetic_operator.php

---
+++ PHP/arithmetic_operator.php
@@ -0,0 +1,16 @@
+<?php
+  $num1 = 8;
+  $num2 = 6;
+
+  //Addition
+  echo $num1 + $num2; //14
+
+  //Subtraction
+  echo $num1 - $num2; //2
+
+  //Multiplication
+  echo $num1 * $num2; //48
+
+  //Division
+   echo $num1 / $num2; //1.33333333333
+?>
