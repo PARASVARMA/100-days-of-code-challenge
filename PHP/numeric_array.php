74ae809b57f47d9037eab2980827a6c175bceb7b
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-13 21:30:02 +0530

    Create numeric_array.php

---
+++ PHP/numeric_array.php
@@ -0,0 +1,9 @@
+<?php
+$myArray[0] = "John";
+$myArray[1] = "<strong>PHP</strong>";
+$myArray[2] = 21;
+
+echo "$myArray[0] is $myArray[2] and knows $myArray[1]";
+
+// Outputs "John is 21 and knows PHP"
+?>
