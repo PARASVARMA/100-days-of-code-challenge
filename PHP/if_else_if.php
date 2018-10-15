80eb10989e2b619d2c4c7ed3f4e25070dfce0248
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-15 21:35:44 +0530

    Create if_else_if.php

---
+++ PHP/if_else_if.php
@@ -0,0 +1,14 @@
+//if else if statement:-
+<?php
+$age = 21;
+
+if ($age<=13) {
+   echo "Child.";
+} elseif ($age>13 && $age<19) {
+   echo "Teenager";
+} else {
+   echo "Adult";
+}
+
+//Outputs "Adult"
+?>
