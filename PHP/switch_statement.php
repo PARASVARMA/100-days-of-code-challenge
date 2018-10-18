b3a95d62eb843efbba5fd3e902515747c1c3eb4d
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-18 18:17:13 +0530

    Create switch_statement.php

---
+++ PHP/switch_statement.php
@@ -0,0 +1,14 @@
+//Switch statement:-
+$x=5;
+switch ($x) {
+  case 1:
+    echo "One";
+    break;
+  case 2:
+    echo "Two";
+    break;
+  default:
+    echo "No match";
+}
+
+//Outputs "No match"
