fe74244fb548981a94847eec0b348cccb649ec19
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-18 18:19:56 +0530

    Create break_statement.php

---
+++ PHP/break_statement.php
@@ -0,0 +1,14 @@
+//Break statement:-
+$x=1;
+switch ($x) {
+  case 1:
+    echo "One";
+  case 2:
+    echo "Two";
+  case 3:
+    echo "Three";
+  default:
+    echo "No match";
+}
+
+//Outputs "OneTwoThreeNo match"
