a2b89669b63d986dbac50db679bbb4b35c982b77
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-24 18:00:07 +0530

    Create form.php

---
+++ PHP/form.php
@@ -0,0 +1,5 @@
+<form action="first.php" method="post">
+  <p>Name: <input type="text" name="name" /></p>
+  <p>Age: <input type="text" name="age" /></p>
+  <p><input type="submit" name="submit" value="Submit" /></p>
+</form>
