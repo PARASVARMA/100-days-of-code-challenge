d594d031d2c75a3b624cdb3e2d50dfb63f403c64
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-10-28 22:42:08 +0530

    Create append_form.php

---
+++ PHP/append_form.php
@@ -0,0 +1,13 @@
+//Appending to a file for a form:-
+<?php
+if(isset($_POST['text'])) {
+  $name = $_POST['text'];
+  $handle = fopen('names.txt', 'a');
+  fwrite($handle, $name."\n");
+  fclose($handle); 
+}
+?>
+<form method="post">
+  Name: <input type="text" name="text" /> 
+  <input type="submit" name="submit" />
+</form>
